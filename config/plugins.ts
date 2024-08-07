5486890ae19865d0cff58c96cc58be0de9094e29
Vadim Ostafiychuk <vadim.ostafiychuk@outlook.com>
2024-08-07 16:29:38 +0300

    init

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 20,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
